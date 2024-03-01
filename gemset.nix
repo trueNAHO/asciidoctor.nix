8b1fdfca6d8e916dde9c477e6a10d71c128bb2ee
trueNAHO <90870942+trueNAHO@users.noreply.github.com>
2024-03-01 00:41:24 +0000

    ci(gemfile): update all inputs

--- gemset.nix
+++ gemset.nix
@@ -4,10 +4,10 @@
     platforms = [];
     source = {
       remotes = ["https://rubygems.org"];
-      sha256 = "0yblqlbix3is5ihiqrpbfazb44in7ichfkjzdbsqibp48paanpl3";
+      sha256 = "00612l3n4swjyv0i8kg673dhq289vgz1gswbscr0jcmas9lqq4q7";
       type = "gem";
     };
-    version = "2.0.20";
+    version = "2.0.21";
   };
   asciidoctor-revealjs = {
     dependencies = ["asciidoctor"];
