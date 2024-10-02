7d9b1754b168b81d1b0cda48cfac1257e61cc200
NAHO <90870942+trueNAHO@users.noreply.github.com>
2024-10-02 10:23:36 +0200

    flake: lib: convert template repository to library
    Convert this template repository to a library to simplify downstream
    updating the upstream dependency.
    
    To reduce maintencance, this change temporarily clears the README.adoc
    and adds a very simple About section.

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Reproducible and Deployable Asciidoctor Template";
+  description = "Reproducible Asciidoctor Library";
 
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
@@ -27,36 +27,23 @@
       system: let
         lib = pkgs.lib;
 
-        nonDefaultPackages =
-          lib.filterAttrs
-          (name: _: !lib.hasPrefix "default" name)
-          inputs.self.packages.${system};
-
         pkgs = inputs.nixpkgs.legacyPackages.${system};
       in {
-        checks = builtins.foldl' lib.attrsets.unionOfDisjoint {} [
-          {
-            git-hooks = inputs.git-hooks.lib.${system}.run {
-              hooks = {
-                alejandra = {
-                  enable = true;
-                  settings.verbosity = "quiet";
-                };
-
-                typos.enable = true;
-                yamllint.enable = true;
+        checks = {
+          git-hooks = inputs.git-hooks.lib.${system}.run {
+            hooks = {
+              alejandra = {
+                enable = true;
+                settings.verbosity = "quiet";
               };
 
-              src = ./.;
+              typos.enable = true;
+              yamllint.enable = true;
             };
-          }
 
-          (
-            lib.attrsets.concatMapAttrs
-            (name: value: {"package-${name}" = value;})
-            nonDefaultPackages
-          )
-        ];
+            src = ./.;
+          };
+        };
 
         devShells.default = pkgs.mkShell {
           inherit (inputs.self.checks.${system}.git-hooks) shellHook;
@@ -70,7 +57,7 @@
 
         formatter = pkgs.alejandra;
 
-        packages = let
+        lib = let
           asciidoctor = {
             command,
             commandOptions ? {},
@@ -79,7 +66,7 @@
             name,
             out ? "${builtins.placeholder "out"}/share/doc",
             outputFile,
-            src ? ./src,
+            src,
           }:
             pkgs.stdenvNoCC.mkDerivation (
               lib.attrsets.unionOfDisjoint
@@ -138,6 +125,11 @@
             ];
           };
 
+          nonDefaultPackages = args:
+            lib.filterAttrs
+            (name: _: !lib.hasPrefix "default" name)
+            (inputs.self.lib.${system}.packages args);
+
           packageName = name: "asciidoctor-nix-${name}";
 
           presentation = {revealJsDir, ...} @ args:
@@ -162,83 +154,106 @@
               // (builtins.removeAttrs args ["revealJsDir"])
             );
         in {
-          default = pkgs.buildEnv {
-            name = packageName "default";
-            paths = lib.attrsets.attrValues nonDefaultPackages;
-          };
+          packages = args: {
+            default = pkgs.buildEnv {
+              name = packageName (args.name or "default");
 
-          defaultExternal = pkgs.buildEnv {
-            name = packageName "default-external";
-
-            paths = lib.attrsets.attrValues (
-              lib.attrsets.filterAttrs
-              (name: _: !lib.hasSuffix "Local" name)
-              nonDefaultPackages
-            );
-          };
-
-          defaultLocal = pkgs.buildEnv {
-            name = packageName "default-local";
-
-            paths = lib.attrsets.attrValues (
-              lib.attrsets.filterAttrs
-              (name: _: !lib.hasSuffix "External" name)
-              nonDefaultPackages
-            );
-          };
-
-          docbook = asciidoctor {
-            command = pkgs.asciidoctor.meta.mainProgram;
-            commandOptions = asciidoctorRequire;
-            name = "docbook";
-            outputFile = "main.xml";
-          };
-
-          html = asciidoctor {
-            command = pkgs.asciidoctor.meta.mainProgram;
-            commandOptions = asciidoctorRequire;
-            name = "html";
-            outputFile = "index.html";
-          };
-
-          manpage = let
-            sectionNumber = toString 7;
-          in
-            asciidoctor {
-              command = pkgs.asciidoctor.meta.mainProgram;
-
-              commandOptions =
-                lib.attrsets.unionOfDisjoint
-                asciidoctorRequire
-                {backend = "manpage";};
-
-              extraOptions.outputs = ["out" "man"];
-              name = "manpage";
-
-              out = "${
-                builtins.placeholder "man"
-              }/share/man/man${sectionNumber}";
-
-              outputFile = "main.${sectionNumber}";
+              paths = lib.attrsets.attrValues (
+                nonDefaultPackages (builtins.removeAttrs args ["name"])
+              );
             };
 
-          pdf = asciidoctor {
-            command = "${pkgs.asciidoctor.meta.mainProgram}-pdf";
-            commandOptions = asciidoctorRequire;
-            name = "pdf";
-            outputFile = "main.pdf";
-          };
+            defaultExternal = pkgs.buildEnv {
+              name = packageName (args.name or "default-external");
 
-          presentationExternal = presentation {
-            name = "presentation-external";
-            outputFile = "presentation_external.html";
-            revealJsDir = "https://cdn.jsdelivr.net/npm/reveal.js@5.0.4";
-          };
+              paths = lib.attrsets.attrValues (
+                lib.attrsets.filterAttrs
+                (name: _: !lib.hasSuffix "Local" name)
+                (nonDefaultPackages (builtins.removeAttrs args ["name"]))
+              );
+            };
 
-          presentationLocal = presentation {
-            name = "presentation-local";
-            outputFile = "presentation_local.html";
-            revealJsDir = inputs.reveal-js.outPath;
+            defaultLocal = pkgs.buildEnv {
+              name = packageName (args.name or "default-local");
+
+              paths = lib.attrsets.attrValues (
+                lib.attrsets.filterAttrs
+                (name: _: !lib.hasSuffix "External" name)
+                (nonDefaultPackages (builtins.removeAttrs args ["name"]))
+              );
+            };
+
+            docbook = asciidoctor (
+              {
+                command = pkgs.asciidoctor.meta.mainProgram;
+                commandOptions = asciidoctorRequire;
+                name = "docbook";
+                outputFile = "main.xml";
+              }
+              // args
+            );
+
+            html = asciidoctor (
+              {
+                command = pkgs.asciidoctor.meta.mainProgram;
+                commandOptions = asciidoctorRequire;
+                name = "html";
+                outputFile = "index.html";
+              }
+              // args
+            );
+
+            manpage = let
+              sectionNumber = toString 7;
+            in
+              asciidoctor (
+                {
+                  command = pkgs.asciidoctor.meta.mainProgram;
+
+                  commandOptions =
+                    lib.attrsets.unionOfDisjoint
+                    asciidoctorRequire
+                    {backend = "manpage";};
+
+                  extraOptions.outputs = ["out" "man"];
+                  name = "manpage";
+
+                  out = "${
+                    builtins.placeholder "man"
+                  }/share/man/man${sectionNumber}";
+
+                  outputFile = "main.${sectionNumber}";
+                }
+                // args
+              );
+
+            pdf = asciidoctor (
+              {
+                command = "${pkgs.asciidoctor.meta.mainProgram}-pdf";
+                commandOptions = asciidoctorRequire;
+                name = "pdf";
+                outputFile = "main.pdf";
+              }
+              // args
+            );
+
+            presentationExternal = presentation (
+              {
+                name = "presentation-external";
+                outputFile = "presentation_external.html";
+                revealJsDir = "https://cdn.jsdelivr.net/npm/reveal.js@5.0.4";
+              }
+              // args
+            );
+
+            presentationLocal = presentation (
+              {
+                name = "presentation-local";
+                outputFile = "presentation_local.html";
+                revealJsDir = inputs.reveal-js.outPath;
+              }
+              // args
+            );
           };
         };
       }
