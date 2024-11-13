trueNAHO/asciidoctor.nix

commit a212394dd06679212f4fa072349d4bb811547458

flake: mkOutputs: increase Asciidoctor failure level
Link: https://docs.asciidoctor.org/asciidoc/latest/attributes/unresolved-references

flake.nix
@@ -128,6 +128,7 @@
                                       {
                                         attribute = lib.flatten (
                                           [
+                                            "attribute-missing=error"
                                             "ditaa-format=svg"
                                             "mathematical-format=svg"
                                             "plantuml-format=svg"

commit 586c0f18b162325aa3304aeb77944fe7e6899ef4

flake: mkOutputs: increase failure level and fix attribute-missing
Fixes: a212394dd066 ("flake: mkOutputs: increase Asciidoctor failure level")

flake.nix
@@ -202,7 +202,7 @@
                                               format = "svg";
                                             in
                                               [
-                                                "attribute-missing=error"
+                                                "attribute-missing=warn"
                                                 "ditaa-format=${format}"
                                                 "mathematical-format=${format}"
                                                 "plantuml-format=${format}"
@@ -215,6 +215,7 @@
                                               );
 
                                             destination-dir = out;
+                                            failure-level = "WARN";
                                             out-file = outputFile;
                                           }
 