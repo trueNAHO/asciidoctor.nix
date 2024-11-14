trueNAHO/asciidoctor.nix

commit a45884d5b8b0b429c18f01b9358639490ff028ed

flake: mkOutputs: support asciidoctor-bibtext plugin

flake.nix
@@ -203,6 +203,7 @@
                                             in
                                               [
                                                 "attribute-missing=warn"
+                                                "bibtex-throw"
                                                 "ditaa-format=${format}"
                                                 "mathematical-format=${format}"
                                                 "plantuml-format=${format}"
@@ -246,7 +247,7 @@
                           asciidoctorRequire =
                             map
                             (library: "asciidoctor-${library}")
-                            ["diagram" "mathematical"];
+                            ["bibtex" "diagram" "mathematical"];
 
                           nonDefaultPackages = args:
                             lib.filterAttrs

commit 1fdd2ae7b73fa15f414d5ea3d2c619bf6497a4a9

flake: mkOutputs: conditionally require asciidoctor-bibtext plugin
Require the bibtex-file plugin only when its required bibtex-file
attribute is defined to prevent the following error:

    Error: bibtex-file is not set and automatic search failed

Fixes: a45884d5b8b0 ("flake: mkOutputs: support asciidoctor-bibtext plugin")

flake.nix
@@ -246,11 +246,6 @@
                               ]
                             );
 
-                          asciidoctorRequire =
-                            map
-                            (library: "asciidoctor-${library}")
-                            ["bibtex" "diagram" "mathematical"];
-
                           packageName = name: "asciidoctor-nix-${name}";
 
                           presentation = {revealJsDir, ...} @ args:
@@ -278,6 +273,23 @@
                             );
                         in
                           args: let
+                            asciidoctorRequire =
+                              map
+                              (library: "asciidoctor-${library}")
+                              (
+                                ["diagram" "mathematical"]
+                                ++ lib.optional
+                                (
+                                  builtins.any
+                                  (
+                                    commandOption:
+                                      lib.hasPrefix "bibtex-file" commandOption
+                                  )
+                                  args.commandOptions.attribute or []
+                                )
+                                "bibtex"
+                              );
+
                             nonDefaultPackages =
                               lib.filterAttrs
                               (name: _: !lib.hasPrefix "default" name)