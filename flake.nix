trueNAHO/asciidoctor.nix

commit 64ff39ffba1312eb350dfe77d0f981b244287705

flake: mkOutputs: packages: manpage: remove to increase compatibility
Remove manpage format to increase package compatibility and reduce code
complexity of catching attribute-missing warnings.

flake.nix
@@ -154,7 +154,7 @@
                             inputFile ? "main.adoc",
                             lastModified ? null,
                             name,
-                            out ? "${builtins.placeholder "out"}/share/doc",
+                            out ? builtins.placeholder "out",
                             outputFile,
                             src,
                           }:
@@ -334,33 +334,6 @@
                                 args
                               ]
                             );
-
-                            manpage = let
-                              sectionNumber = toString 7;
-                            in
-                              asciidoctor (
-                                lib.asciidoctor.mergeAttrsMkMerge [
-                                  {
-                                    command = pkgs.asciidoctor.meta.mainProgram;
-
-                                    commandOptions = {
-                                      backend = "manpage";
-                                      require = asciidoctorRequire;
-                                    };
-
-                                    extraOptions.outputs = ["out" "man"];
-                                    name = "manpage";
-
-                                    out = "${
-                                      builtins.placeholder "man"
-                                    }/share/man/man${sectionNumber}";
-
-                                    outputFile = "main.${sectionNumber}";
-                                  }
-
-                                  args
-                                ]
-                              );
 
                             pdf = asciidoctor (
                               lib.asciidoctor.mergeAttrsMkMerge [

commit 28c175169d14cd77a140a7ee75371f12bfbcbccf

flake: mkOutputs: packages: change default Asciidoctor output directory
Fixes: 64ff39ffba13 ("flake: mkOutputs: packages: manpage: remove to increase compatibility")

flake.nix
@@ -155,7 +155,7 @@
                             inputFile ? "main.adoc",
                             lastModified ? null,
                             name,
-                            out ? builtins.placeholder "out",
+                            out ? "${builtins.placeholder "out"}/share/doc",
                             outputFile,
                             src,
                           }: