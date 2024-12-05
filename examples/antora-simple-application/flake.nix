trueNAHO/asciidoctor.nix

commit beff29abbb5494f29a20c4d456fbcfd91d1848ac

examples: antora-simple-application: init

examples/antora-simple-application/flake.nix
@@ -1,0 +1,67 @@
+{
+  description = "Simple Asciidoctor Example Based on Antora's Structure with an Application";
+
+  inputs = {
+    asciidoctor-nix.url = "github:trueNAHO/asciidoctor.nix";
+    flake-utils.follows = "asciidoctor-nix/flake-utils";
+    nixpkgs.follows = "asciidoctor-nix/nixpkgs";
+  };
+
+  outputs = inputs:
+    inputs.flake-utils.lib.eachDefaultSystemPassThrough (
+      system: let
+        lib = inputs.asciidoctor-nix.mkLib pkgs.lib;
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+      in
+        lib.asciidoctor.mergeAttrsMkMerge [
+          (
+            inputs.flake-utils.lib.eachDefaultSystem (
+              _: {
+                packages = {
+                  application-default = let
+                    mainProgram = "application";
+                  in
+                    pkgs.stdenv.mkDerivation {
+                      buildPhase = "$CC ${mainProgram}.c -o ${mainProgram}";
+
+                      installPhase = ''
+                        mkdir --parent $out/bin
+                        cp ${mainProgram} $out/bin
+                      '';
+
+                      name = mainProgram;
+                      src = src/application;
+                    };
+
+                  default = pkgs.buildEnv {
+                    name = "default";
+
+                    paths = lib.attrsets.attrValues (
+                      lib.filterAttrs
+                      (package: _: builtins.match ".*-default" package != null)
+                      inputs.self.packages.${system}
+                    );
+                  };
+                };
+              }
+            )
+          )
+
+          (
+            inputs.asciidoctor-nix.mkOutputs {
+              checks.hooks = {
+                clang-format.enable = true;
+                clang-tidy.enable = true;
+              };
+
+              packages = {
+                inherit (inputs.self) lastModified;
+
+                name = "report";
+                src = src/report;
+              };
+            }
+          )
+        ]
+    );
+}

examples/antora-simple-application/.envrc
@@ -1,0 +1,1 @@
+use flake

examples/antora-simple-application/.github/dependabot.yml
@@ -1,0 +1,13 @@
+---
+version: 2
+
+updates:
+  - package-ecosystem: github-actions
+
+    commit-message:
+      prefix: "github: workflows"
+
+    directory: /
+
+    schedule:
+      interval: daily

examples/antora-simple-application/.github/workflows/deploy.yml
@@ -1,0 +1,26 @@
+---
+name: Deploy
+
+on:  # yamllint disable-line rule:truthy
+  push:
+    tags:
+      - 'v*.*.*'
+
+jobs:
+  deploy:
+    runs-on: ubuntu-22.04
+
+    permissions:
+      contents: write
+
+    steps:
+      - uses: actions/checkout@v4
+      - uses: DeterminateSystems/nix-installer-action@v16
+      - uses: DeterminateSystems/magic-nix-cache-action@v8
+
+      - run: nix build
+
+      - uses: softprops/action-gh-release@v2
+        with:
+          files: result/**/*
+          token: ${{ secrets.GITHUB_TOKEN }}

examples/antora-simple-application/.github/workflows/nix_flake_check.yml
@@ -1,0 +1,15 @@
+---
+name: Nix Flake Check
+
+on:  # yamllint disable-line rule:truthy
+  push:
+
+jobs:
+  nix-flake-check:
+    runs-on: ubuntu-22.04
+
+    steps:
+      - uses: actions/checkout@v4
+      - uses: DeterminateSystems/nix-installer-action@v16
+      - uses: DeterminateSystems/magic-nix-cache-action@v8
+      - run: nix flake check

examples/antora-simple-application/.github/workflows/nix_flake_update.yml
@@ -1,0 +1,26 @@
+---
+name: Nix Flake Update
+
+on:  # yamllint disable-line rule:truthy
+  workflow_dispatch:
+
+  schedule:
+    - cron: "0 0 1 * *"
+
+jobs:
+  nix-flake-update:
+    runs-on: ubuntu-22.04
+
+    permissions:
+      contents: write
+      pull-requests: write
+
+    steps:
+      - uses: actions/checkout@v4
+      - uses: DeterminateSystems/nix-installer-action@v16
+      - uses: DeterminateSystems/magic-nix-cache-action@v8
+
+      - uses: DeterminateSystems/update-flake-lock@v24
+        with:
+          pr-labels: dependencies
+          pr-title: "flake: inputs: update all"

examples/antora-simple-application/.gitignore
@@ -1,0 +1,4 @@
+/.direnv/
+/.pre-commit-config.yaml
+result
+result-*

examples/antora-simple-application/LICENSE
@@ -1,0 +1,21 @@
+MIT License
+
+Copyright (c) 2024 NAHO
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.

commit ef88116785fd3f3f0dfcaa99da2fb877a1231502

antora-simple-application: add missing inputFile declaration
Fixes: beff29abbb54 ("examples: antora-simple-application: init")

examples/antora-simple-application/flake.nix
@@ -77,6 +77,7 @@
               packages = {
                 inherit (inputs.self) lastModified;
 
+                inputFile = "pages/index.adoc";
                 name = "report";
                 src = src/report;
               };