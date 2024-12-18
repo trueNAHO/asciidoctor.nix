trueNAHO/asciidoctor.nix

commit 228efd373df18c9aad7a82d6e79d3791b7e40502

flake: mkOutputs: packages: set root to current working directory
Set the root Asciidoctor attribute to the runtime's current working
directory instead of the derivation's src Nix path to support advanced
use cases where these two are not the same, like leveraging the
postPatch phase.

This may also increase performance by avoiding copying src into the Nix
store.

flake.nix
@@ -185,36 +185,8 @@
                             {
                               inherit src;
 
-                              buildPhase = ''
-                                ${
-                                  lib.optionalString
-                                  (
-                                    builtins.elem
-                                    "asciidoctor-mathematical"
-                                    commandOptions.require or []
-                                  )
-                                  ''
-                                    export FONTCONFIG_FILE="${
-                                      pkgs.makeFontsConf {
-                                        fontDirectories = [
-                                          "${pkgs.lyx}/share/lyx/fonts"
-                                        ];
-                                      }
-                                    }"
-                                  ''
-                                }
-
-                                ${
-                                  lib.optionalString
-                                  (lastModified != null)
-                                  ''
-                                    export SOURCE_DATE_EPOCH="${
-                                      toString lastModified
-                                    }"
-                                  ''
-                                }
-
-                                ${command} ${
+                              buildPhase = let
+                                commandLineOptions =
                                   lib.cli.toGNUCommandLineShell
                                   {}
                                   (
@@ -248,8 +220,40 @@
 
                                       commandOptions
                                     ]
+                                  );
+                              in ''
+                                ${
+                                  lib.optionalString
+                                  (
+                                    builtins.elem
+                                    "asciidoctor-mathematical"
+                                    commandOptions.require or []
                                   )
-                                } ${lib.escapeShellArg inputFile}
+                                  ''
+                                    export FONTCONFIG_FILE="${
+                                      pkgs.makeFontsConf {
+                                        fontDirectories = [
+                                          "${pkgs.lyx}/share/lyx/fonts"
+                                        ];
+                                      }
+                                    }"
+                                  ''
+                                }
+
+                                ${
+                                  lib.optionalString
+                                  (lastModified != null)
+                                  ''
+                                    export SOURCE_DATE_EPOCH="${
+                                      toString lastModified
+                                    }"
+                                  ''
+                                }
+
+                                ${command} \
+                                  ${commandLineOptions} \
+                                  --attribute root="$PWD" \
+                                  ${lib.escapeShellArg inputFile}
                               '';
 
                               installPhase = ''

commit 9c354fd89b6c7352ae4d10e441c5ad5517eff8c1

flake: mkOutputs: packages: do not set root to temporary directory
Set the runtime's current working directory to a Nix store path instead
of a temporary /build directory to avoid inaccessible paths in
presentation packages.

The tests are passing because they use valid /build paths, but this will
fail at run time when accessing an invalidated /build directory.

Fixes: 228efd373df1 ("flake: mkOutputs: packages: set root to current working directory")

flake.nix
@@ -251,7 +251,7 @@
 
                                 ${command} \
                                   ${commandLineOptions} \
-                                  --attribute root="$PWD" \
+                                  --attribute root="$src" \
                                   ${lib.escapeShellArg inputFile}
                               '';
 