trueNAHO/asciidoctor.nix

commit 716858e129e70a1e4b9c701d4ebe01f54b93d0a9

flake: mkOutputs: packages: increase external presentation deployability
Increase the external presentation deployability by bundling all
potentially required external resources. This is necessary because it is
unfeasible to inline all referenced external resources.

While commit 9c354fd89b6c ("flake: mkOutputs: packages: do not set root
to temporary directory") fixes local presentation deployability, this
change fixes external presentation deployability.

flake.nix
@@ -252,8 +252,8 @@
                                 }
 
                                 ${command} \
-                                  ${commandLineOptions} \
                                   --attribute root="$src" \
+                                  ${commandLineOptions} \
                                   ${lib.escapeShellArg inputFile}
 
                                 runHook postBuild
@@ -403,11 +403,56 @@
 
                       "${prefix.hyphen}presentation-external" = presentation (
                         lib.asciidoctor.mergeAttrsMkMerge [
-                          {
-                            name = "presentation-external";
-                            outputFile = "presentation_external.html";
-                            revealJsDir = "https://cdn.jsdelivr.net/npm/reveal.js@5.1.0";
-                          }
+                          (
+                            lib.fix (
+                              self: {
+                                commandOptions.attribute = ["root=./"];
+                                name = "presentation-external";
+                                out = "${builtins.placeholder "out"}/share/doc";
+                                outputFile = "presentation_external.html";
+
+                                extraOptions = {
+                                  nativeBuildInputs = [pkgs.zip];
+
+                                  postInstall = let
+                                    out = lib.escapeShellArg self.out;
+                                  in ''
+                                    directory="$(mktemp --directory)"
+                                    mv ${out}/{.,}* "$directory"
+                                    rm --recursive "$out"
+                                    mkdir --parents "$out" ${out}
+
+                                    # TODO: Replace --update=none with
+                                    # --update=none-fail once commit
+                                    # de49e993ea8b [1] ("cp: actually support
+                                    # --update=none-fail") is available.
+                                    #
+                                    # [1]: https://github.com/coreutils/coreutils/commit/de49e993ea8b6dcdf6cada3c0f44a6371514f952
+                                    cp \
+                                      --recursive \
+                                      --update=none \
+                                      "$src/." \
+                                      "$directory"
+
+                                    output_file=${out}/${
+                                      lib.escapeShellArg prefix.underscore
+                                    }${
+                                      lib.escapeShellArg self.outputFile
+                                    }
+
+                                    cd "$directory"
+
+                                    zip \
+                                      --recurse-paths \
+                                      "''${output_file%.*}.zip" \
+                                      .
+                                  '';
+                                };
+
+                                revealJsDir = "https://cdn.jsdelivr.net/npm/reveal.js@5.1.0";
+                              }
+                            )
+                          )
 
                           args
                         ]

commit fa415b6bb2f5306d202e0803ca9d325a9e85836e

flake: mkOutputs: packages: fix relative paths in external presentations
The root Asciidoctor attribute for external presentations causes build
failures because it refers to the directory containing the evaluated
file, instead of the working directory.

This change reverts the root attribute to a Nix store path, aligning
with commit 9c354fd89b6c ("flake: mkOutputs: packages: do not set root
to temporary directory"), and replaces it with '.' afterwards.

Fixes: 716858e129e7 ("flake: mkOutputs: packages: increase external presentation deployability")

flake.nix
@@ -406,17 +406,26 @@
                           (
                             lib.fix (
                               self: {
-                                commandOptions.attribute = ["root=./"];
                                 name = "presentation-external";
                                 out = "${builtins.placeholder "out"}/share/doc";
                                 outputFile = "presentation_external.html";
 
-                                extraOptions = {
+                                extraOptions = let
+                                  out = lib.escapeShellArg self.out;
+
+                                  outputFile = "${out}/${
+                                    lib.escapeShellArg prefix.underscore
+                                  }${
+                                    lib.escapeShellArg self.outputFile
+                                  }";
+                                in {
                                   nativeBuildInputs = [pkgs.zip];
 
-                                  postInstall = let
-                                    out = lib.escapeShellArg self.out;
-                                  in ''
+                                  postBuild = ''
+                                    sed --in-place "s@$src@.@g" ${outputFile}
+                                  '';
+
+                                  postInstall = ''
                                     directory="$(mktemp --directory)"
                                     mv ${out}/{.,}* "$directory"
                                     rm --recursive "$out"
@@ -434,11 +443,7 @@
                                       "$src/." \
                                       "$directory"
 
-                                    output_file=${out}/${
-                                      lib.escapeShellArg prefix.underscore
-                                    }${
-                                      lib.escapeShellArg self.outputFile
-                                    }
+                                    output_file=${outputFile}
 
                                     cd "$directory"
 