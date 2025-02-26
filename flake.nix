trueNAHO/asciidoctor.nix

commit 2e427fd7ece3a379b41b1bad9bda752130b2757e

flake: mkOutputs: enable Rouge source highlighter

flake.nix
@@ -208,6 +208,7 @@
                                                 "mathematical-format=${format}"
                                                 "plantuml-format=${format}"
                                                 "root=${src}"
+                                                "source-highlighter=rouge"
                                               ]
                                               ++ (
                                                 lib.optional

commit 4677a426878f4b8a27b7529de8b8576f25c60214

flake: mkOutputs: use Highlight.js source highlighter on presentations
Use the Highlight.js source highlighter on presentations to prevent the
following error:

    asciidoctor: WARNING: optional gem 'rouge' is not available.
    Functionality disabled.

Fixes: 2e427fd7ece3 ("flake: mkOutputs: enable Rouge source highlighter")

flake.nix
@@ -316,6 +316,7 @@
 
                               commandOptions.attribute = [
                                 "revealjsdir=${revealJsDir}"
+                                "source-highlighter=highlight.js"
                               ];
 
                               extraOptions.nativeBuildInputs =